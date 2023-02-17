54d665e3c511a992ff67c6ef5a05491648e2c157
minindu0 <mw8511@live.com>
2023-02-17 14:34:44 -0600

    test

--- src/main.rs
+++ src/main.rs
@@ -50,11 +50,6 @@ fn main() {
     else {
         seqvec = get_random_sequences_from_generator(2000, 10);
     }
-    let file = File::open(FILENAME).expect("no such file");
-    let buf = BufReader::new(file);
-    let seqvec: Vec<String> = buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect();
     check_the_alignment_pacbio(seqvec);
     //run(seqvec);
 }
@@ -66,14 +61,14 @@ fn  check_the_alignment_pacbio (seqvec: Vec<String>) {
             println!("pacbio sequence");
         }
         let score = |a: u8, b: u8| if a == b { MATCH } else { MISMATCH };
-        let mut aligner = bio::alignment::pairwise::Aligner::with_capacity(seqvec[0].len(), seq.len(), GAP_OPEN, GAP_EXTEND, &score);
+        let mut aligner = bio::alignment::pairwise::Aligner::with_capacity(seqvec[1].len(), seq.len(), GAP_OPEN, GAP_EXTEND, &score);
         let mut test = seq.as_bytes().clone().to_vec();
         let alignment = aligner.local(seqvec[0].as_bytes(), &test);
 
         println!("sequence {} score: \t{}", index, alignment.score);
 
         let score = |a: u8, b: u8| if a == b { MATCH } else { MISMATCH };
-        let mut aligner = bio::alignment::pairwise::Aligner::with_capacity(seqvec[0].len(), seq.len(), GAP_OPEN, GAP_EXTEND, &score);
+        let mut aligner = bio::alignment::pairwise::Aligner::with_capacity(seqvec[1].len(), seq.len(), GAP_OPEN, GAP_EXTEND, &score);
         test.reverse();
         let alignment = aligner.local(seqvec[0].as_bytes(), &test);
         
